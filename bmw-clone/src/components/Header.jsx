98e2c8dbca20f0cc07f40178bf2f3e764699c023
DavideCruciani <cruciani240203@gmail.com>
2023-05-01 00:39:50 +0200

    hero and header sizes fixed

--- bmw-clone/src/components/Header.jsx
+++ bmw-clone/src/components/Header.jsx
@@ -9,22 +9,22 @@ const Header = () => {
 
         <nav className='flex justify-between h-28 border-b-2'>
         <div className='flex items-center w-full'>
-          <img className='mr-8 w-[4.25rem] cursor-pointer' src={Logo} alt="BMW logo" />
-          <ul className='flex h-full font-Poppins font-semibold text-white text-xl'>
-            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>Modelli</li>
-            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>All Electric</li>
-            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>Configura la tua BMW</li>
-            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>Shop Online</li>
-            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>More BMW</li>
+          <img className='mr-8 w-[3.25rem] cursor-pointer' src={Logo} alt="BMW logo" />
+          <ul className='flex h-full font-Poppins font-semibold text-white text-base'>
+            <li className='p-3 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>Modelli</li>
+            <li className='p-3 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>All Electric</li>
+            <li className='p-3 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>Configura la tua BMW</li>
+            <li className='p-3 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>Shop Online</li>
+            <li className='p-3 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'>More BMW</li>
           </ul>
         </div>
 
         <div>
           <ul className='flex h-full'>
-            <li className='p-6 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faUser} style={{color: "#ffffff",}} size='lg' /></li>
-            <li className='p-6 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faShoppingCart} style={{color: "#ffffff",}} size='lg' /></li>
-            <li className='p-6 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faLocationDot} style={{color: "#ffffff",}} size='lg' /></li>
-            <li className='p-6 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#ffffff",}} size='lg' /></li>
+            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faUser} style={{color: "#ffffff",}} size='md' /></li>
+            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faShoppingCart} style={{color: "#ffffff",}} size='md' /></li>
+            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faLocationDot} style={{color: "#ffffff",}} size='md' /></li>
+            <li className='p-4 h-full flex items-center cursor-pointer border-b-[6px] border-t-[6px] border-transparent hover:border-b-[#1c69d4]'><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#ffffff",}} size='md' /></li>
           </ul>
         </div>
         </nav>
--- bmw-clone/src/components/Hero.jsx
+++ bmw-clone/src/components/Hero.jsx
@@ -4,32 +4,32 @@ import smallLogo from '../assets/bmw-small-logo.png'
 
 const Hero = () => {
   return (
-    <section className='bg-[url(/src/assets/bmw-hero.webp)] h-[115vh] bg-no-repeat'>
+    <section className='bg-[url(/src/assets/bmw-hero.webp)] h-[115vh] bg-cover bg-no-repeat'>
       <div className='w-full h-full bg-gradient-70deg from-[#0006] to-transparent'>
         <Header />
         <div className='px-32 w-full pt-6'>
 
-          <div className='mb-72'>
-            <p className='text-white font-Poppins text-xl'><span className='font-semibold'>Piacere</span> di guidare</p>
+          <div className='mb-52'>
+            <p className='text-white font-Poppins text-base'><span className='font-semibold'>Piacere</span> di guidare</p>
           </div>
 
           <div className='px-4'>
 
             <div className='mb-6'>
-              <p className='text-white font-bold font tracking-wider text-xl'>THE</p>
+              <p className='text-white font-bold font tracking-wider text-base'>THE</p>
             </div>
 
             <div className='flex'>
-              <h1 className='text-[20rem] -ml-2 leading-none font-Poppins text-white'>7</h1>
+              <h1 className='text-[18rem] font-light -ml-2 leading-none font-Poppins text-white'>7</h1>
             </div>
 
-            <div className='flex items-center mb-12'>
+            <div className='flex items-center mb-8'>
               <img className='w-14 mr-2' src={smallLogo} alt="BMW logo" />
-              <h2 className='text-[3rem] text-white font-Poppins'>NUOVA BMW SERIE 7 M.</h2>
+              <h2 className='text-[2.25rem] font-light text-white font-Poppins'>NUOVA BMW SERIE 7 M.</h2>
             </div>
 
             <div>
-              <button className='bg-[#1c69d4] hover:bg-[#1653a8] px-56 py-5 text-2xl rounded-md font-Poppins text-white font-semibold'>Scopri di più</button>
+              <button className='bg-[#1c69d4] hover:bg-[#1653a8] px-40 py-4 text-base rounded-sm font-Poppins text-white font-semibold'>Scopri di più</button>
             </div>
           </div>
         </div>
