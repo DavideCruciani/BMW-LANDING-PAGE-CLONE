1fe8b8602a05a632ed3e2800296fedc44ec6aa53
DavideCruciani <cruciani240203@gmail.com>
2023-05-04 23:25:21 +0200

    findyourbmw section done

--- bmw-clone/src/components/FindYourBMW.jsx
+++ bmw-clone/src/components/FindYourBMW.jsx
@@ -7,23 +7,23 @@ const FindYourBMW = () => {
   return (
     <section className='text-center py-16 font-Poppins px-4 md:px-10 xl:px-20 text-[#262626] max-w-[2600px] mx-auto'>
       <div className='px-4'>
-        <h3 className='text-xl font-semibold mb-8'>SHOPPING TOOLS</h3>
-        <h2 className='text-7xl font-light'>TROVA LA TUA BMW.</h2>
+        <h3 className='text-base xl:text-lg 2xl:text-xl font-semibold mb-2 xl:mb-4'>SHOPPING TOOLS</h3>
+        <h2 className='text-4xl xl:text-5xl 2xl:text-7xl font-light'>TROVA LA TUA BMW.</h2>
       </div>
-      <div className='grid grid-cols-3'>
+      <div className='grid grid-cols-1 lg:grid-cols-3'>
         <div className='px-4'>
           <img src={Stock} alt="Trova una Nuova BMW" />
-          <p className='text-2xl mb-16'>Trova una Nuova BMW</p>
+          <p className='text-lg xl:text-xl 2xl:text-2xl mb-10'>Trova una Nuova BMW</p>
           <button className='border-[1px] hover:shadow-buttonBlack border-black px-6 py-4 text-base font-semibold rounded-[3px]'>Vetture disponibili</button>
         </div>
         <div className='px-4'>
           <img src={testDrive} alt="Prenota un Test Drive" />
-          <p className='text-2xl mb-16'>Prenota un Test Drive</p>
+          <p className='text-lg xl:text-xl 2xl:text-2xl mb-10'>Prenota un Test Drive</p>
           <button className='border-[1px] hover:shadow-buttonBlack border-black px-6 py-4 text-base font-semibold rounded-[3px]'>Test Drive</button>
         </div>
         <div className='px-4'>
           <img src={Con} alt="Configura la Tua BMW" />
-          <p className='text-2xl mb-16'>Configura la Tua BMW</p>
+          <p className='text-lg xl:text-xl 2xl:text-2xl mb-10'>Configura la Tua BMW</p>
           <button className='border-[1px] hover:shadow-buttonBlack border-black px-6 py-4 text-base font-semibold rounded-[3px]'>Configuratore & prezzo</button>
         </div>
       </div>
